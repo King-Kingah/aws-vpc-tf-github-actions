6a5ce31783bd35bb9c052aedc0e636c51d37c155
King-Kingah <kingajames2015@gmail.com>
2024-03-12 12:16:05 +0300

    Add update vpc files.

--- Terraform-VPC/modules/vpc/variables.tf
+++ Terraform-VPC/modules/vpc/variables.tf
@@ -1,15 +1,15 @@
 variable "vpc_cidr" {
   description = "VPC CIDR Range"
-  type = string
+  type        = string
 }
 
 variable "subnet_cidr" {
   description = "Subnet CIDRS"
-  type = list(string)
+  type        = list(string)
 }
 
 variable "subnet_names" {
   description = "Subnet Names"
-  type = list(string)
-  default = [ "PublicSubnet1", "PublicSubnet2" ]
+  type        = list(string)
+  default     = ["PublicSubnet1", "PublicSubnet2"]
 }
